ff681aa4276c43eeaceead20e43c0acfa7fa6603
Michael J Maggs <seattleslew@runawayserver.com>
2023-05-15 12:52:21 -0700

    advanced array initial commit

---
+++ topics/advancedArrays.js
@@ -0,0 +1,11 @@
+var array = [ 1, 2, 10, 16 ];
+
+//ForEach
+// multiply each element by some number
+
+const double = [];
+const newArray = array.forEach( ( num ) => {
+    double.push( num * 2 ); //forEach doesn't modify the original array so result needs to be added to new array
+} );
+
+console.log( double );
