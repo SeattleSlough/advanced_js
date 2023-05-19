ffc7b3c1ba195dca085cfaae646f25a2528c0f90
Michael J Maggs <seattleslew@runawayserver.com>
2023-05-18 17:52:19 -0700

    removed variable naming conflict

--- exercises/ES10-Exercise.js
+++ exercises/ES10-Exercise.js
@@ -35,7 +35,7 @@ const arrayOfUsers = Object.entries( users );
 
 
 //#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
-const arrayOfUsers = Object.entries( users ).map( array => {
+const arrayOfUsers1 = Object.entries( users ).map( array => {
     return array.map( item => ( isNaN( item ) ? item : item * 2 ) );
 } );
 
@@ -46,5 +46,5 @@ updatedUsersArray = usersArray.map( ( user ) => [ user[ 0 ], user[ 1 ] * 2 ] );
 
 //#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
 
-const updatedUserObj = Object.fromEntries( arrayOfUsers );
+const updatedUserObj = Object.fromEntries( arrayOfUser1 );
 console.log( updatedUserObj );
