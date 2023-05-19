78740cfe7e5924cc12329736a023543ac28fa853
Michael J Maggs <seattleslew@runawayserver.com>
2023-05-19 11:43:18 -0700

    completed exercises for advanced loops

--- exercises/advancedLoopingExercise.js
+++ exercises/advancedLoopingExercise.js
@@ -1,47 +1,59 @@
-const basket = ['apples', 'oranges', 'grapes'];
+const basket = [ 'apples', 'oranges', 'grapes' ];
 const detailedBasket = {
   apples: 5,
   oranges: 10,
   grapes: 1000
-}
+};
 
 //1
-for (let i = 0; i < basket.length; i++) {
-  console.log(basket[i]);
+for( let i = 0; i < basket.length; i++ ) {
+  console.log( basket[ i ] );
 }
 
 //2
-basket.forEach(item => {
-  console.log(item);
-})
+basket.forEach( item => {
+  console.log( item );
+} );
 
-for (item in detailedBasket) {
-  console.log(item);
+for( item in detailedBasket ) {
+  console.log( item );
 }
 
-for (item of basket) {
-  console.log(item);
+for( item of basket ) {
+  console.log( item );
 }
 
 // Question #1:
 // create a function called biggestNumberInArray() that takes
 // an array as a parameter and returns the biggest number.
-// biggestNumberInArray([-1,0,3,100, 99, 2, 99]) should return 100;
 // Use at least 3 different types of javascript loops to write this:
-const array = [-1,0,3,100, 99, 2, 99] // should return 100
-const array2 = ['a', 3, 4, 2] // should return 4
-const array3 = [] // should return 0
+const array = [ -1, 0, 3, 100, 99, 2, 99 ]; // should return 100
+const array2 = [ 'a', 3, 4, 2 ]; // should return 4
+const array3 = []; // should return 0
+console.log( biggestNumberInArray( array3 ) );
 
-function biggestNumberInArray(arr) {
-
+function biggestNumberInArray( arr ) {
+  let biggest = 0;
+  for( let i = 0; i < arr.length; i++ ) {
+    biggest = ( biggest > arr[ i ] ? biggest : arr[ i ] );
+  }
+  return biggest;
 }
 
-function biggestNumberInArray2(arr) {
-
+function biggestNumberInArray2( arr ) {
+  let biggest = 0;
+  for( item of arr ) {
+    biggest = ( biggest > item ? biggest : item );
+  }
+  return biggest;
 }
 
-function biggestNumberInArray3(arr) {
-
+function biggestNumberInArray3( arr ) {
+  let biggest = 0;
+  arr.forEach( item => {
+    biggest = ( biggest > item ? biggest : item );
+  } );
+  return biggest;
 }
 
 
@@ -51,8 +63,13 @@ amazonBasket = {
   glasses: 1,
   books: 2,
   floss: 100
+};
+
+function checkBasket( basket, lookingFor ) {
+  let inBasket = "No";
+  for( item in basket ) {
+    if( item === lookingFor ) { inBasket = "Yes"; }
+  }
+  return inBasket;
 }
 
-function checkBasket(basket, lookingFor) {
-
-}
